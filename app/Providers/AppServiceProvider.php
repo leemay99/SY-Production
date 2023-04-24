97cfa5e51078241dbc4b8ff7f6f248f414da8c54
leemay99 <118901641+leemay99@users.noreply.github.com>
2023-04-24 21:55:31 +0900

    URLの名前空間を明示

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,8 +26,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-        if(\App::environment(['production']) || \App::environment(['develop'])){
-           \URL::forceScheme('https');
-    }
+        if (env('APP_ENV') === 'production' || env('APP_ENV') === 'develop') {
+            URL::forceScheme('https');
+        }
     }
 }
